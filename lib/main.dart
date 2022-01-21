660b17a472ba90f090abb10ff08f67a1ec322a74
mr cho <kyuseong2001@gmail.com>
2022-01-21 15:08:43 +0700

    '20220121'

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,145 @@
 import 'package:flutter/material.dart';
-import'./style.dart' as style; // 경로
+import './style.dart' as style;
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// todo 변수중복을 막기위해서는 타입을 as 옆에 써주고 변수아래 사용한 값의 앞에 타입을 넣는다.
-// todo 변수 앞에 _ 언더바를 쓰면 다른 파일에서는 절대 사용할 수 없는 변수가 된다.  var _vard1; 이런식으로
-
+//c import './style.dart' as style;
+//c 변수명이 겹치면 as 라는 키워드로 사용한다.
+//c theme: style.theme,
+//c 변수를 다른 파일에서 쓰기 싫으면 _언더바를 변수앞에 붇인다.
 
 void main() {
-  runApp(
-      MaterialApp(
-
-    theme:style.theme,
-          home: MyApp()
-    ),
-  );
+  runApp(MaterialApp(
+    theme: style.theme,
+    home: MyApp(),
+  ));
 }
 
-
-
-
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+  var tab = 0;
+  var map = {'name':'john', 'age':16};
+  var data = [];
+  //c 비어있는 스테이트를 만들어 놓는다.
+
+
+
+
+
+  //c map ['name']; 이름을 불러주는 법
+  //c 불러온 데이터 값이 대괄호[]로 시작하면 무조건 리스트이다.
+
+
+
+  getData() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+    setState(() {  //c set state를 써야 이미지를 저장할 수 있다 변하니까.
+      data = result2;   //c 리절트2의 결과의 값을 Data 라는 변수에 저장한다. 대문자 소문자 잘 봐야 한다.
+    });
+  }
+
+  @override
+  void initState() {    // c initState안에는 async를 붙일수 없게 되어있다.
+    super.initState();
+    getData();
+
+
+   //c 서버랑 데이터를 주고받을때는 문자만 나눌수 있다. json을 리스트, 맵의 형태로 뽑아서 쓰기위해서 변환함수 아래를 써야한다.
+   //c   print(jsonDecode(result.body));
+    //c 리스트 맵의 형태로 뽑아서 써야한다.
+
+
+  } //c myapp 앱이 로드되자마자 서버에 겟요청을 할때
+
+
+
+
+//c 위에 변수에 스테이트에 번호를 지정해놓는다.
+  //c state에 따라 tab이 어떻게 보일지를 지정해 놓는다.
+  //c ontab 함수를 만들어 놓는다. 리스트형태에서 i번째의 버튼을 보여준다.
+  //c 버튼에 번호를 리스트 형태에 보여준다
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Instagram'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add_box_outlined),
+            iconSize: 30,
+          )
+        ],
+      ),
+      body: [Home( data : data ), Text('쇼핑')][tab],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false, //c 아이콘 밑에 텍스트바를 안보이게 하는방법
 
-        title: Text('Instagram',style: TextStyle(color: Colors.black)),
-        actions: [Icon(Icons.add_box_outlined)],),
-      body: Theme(
-          data:ThemeData(
-          ), child:Container(
-
-      )),
+        onTap: (i) {
+          setState(() {
+            tab = i;
+          });
+        },
 
 
 
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          margin: EdgeInsets.all(20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(Icons.home_outlined),
-              Icon(Icons.shopping_bag_outlined),
-            ],
-          ),
-        ),
+
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: 'shop'),
+        ],
       ),
     );
   }
 }
+
+class Home extends StatelessWidget {
+  const Home({Key? key, this.data}) : super(key: key);
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    print(data);
+
+    //c 프린트는 함수같이 생긴곳에서만 쓸수 있다.
+    //c 옆에있는 데이터가 비어있는지 안비어있는지 알려주는 함수
+
+    if (data.isNotEmpty){
+      return ListView.builder(
+          itemCount:3,
+          itemBuilder: (c, i) {
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(data[i]['image']), // c http 인터넷상의 이미지 경로를 넣어야 할때는 Image.network
+                  Text(data[i]['likes'].toString(),),
+                  Text(data[i]['user']),
+                  Text(data[i]['content']),
+                ],
+              ),
+            );
+          });
+    } else{
+      return Text('LODING');
+    }
+  }
+}
+
+
+
+// if 데이터에 뭐가 있으면 보여주세요
--- lib/style.dart
+++ lib/style.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
 
 var theme = ThemeData(
-  textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(
-      backgroundColor: Colors.blueGrey,
-    )
+  //c ThemeData 쓰면 깔끔해보인다는 장점이 있다.
+
+
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+  selectedItemColor: Colors.black,
   ),
 
 
-    iconTheme: IconThemeData(color: Colors.black),
-    appBarTheme: AppBarTheme(
-        color:Colors.white,
-        actionsIconTheme:IconThemeData(color: Colors.black)
+
+
+  iconTheme: IconThemeData(color: Colors.black),
+
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    titleTextStyle: TextStyle(color:Colors.black,
+    fontSize: 20),
+    actionsIconTheme: IconThemeData(
+    color: Colors.black)
     ),
-    textTheme: TextTheme(
-      bodyText2: TextStyle(color:Colors.red),
-      headline6: TextStyle(
+
+
+  //c 모든 텍스트 스타일 지정하는방법 아래
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      backgroundColor: Colors.white54,
+      textStyle: TextStyle(
         color:Colors.black,
-      ),
-    )//todo 위에 씸대에터를 쓰면 깔끔해보인다는 장점이 있다.
+      )
+    )
 
-  //todo text()는 bodyText2 가져다가 씀
-  // listTile()은 Subtitle1
-  // TextButton()은 button
-  // Appbar()는 headline6
+  )
 
 
-);  //todo 소괄호 안에 모든 스타일을 넣을 수 있다.
-// todo 장점은 모든 팀데이터 안에 넣어놓으면 모든 아이콘 스타일이 적용된다.
+
+
+  );
+
+
+
+
+//c text()는 bodyText2 가져다가 씀
+//c listTile()은 Subtitle1
+//c TextButton()은 button
+//c Appbar()는 headline6
+//c 소괄호 안에 모든 스타일을 넣을 수 있다.
+//c 장점은 모든 팀데이터 안에 넣어놓으면 모든 아이콘 스타일이 적용된다.
